14d3bb6c50ed7e325133b629ba778c1c84327807
Mritunjay Gupta <66947124+Mritunjay004@users.noreply.github.com>
2021-01-07 13:29:07 +0530

    Add files via upload

---
+++ src/components/CardList.js
@@ -0,0 +1,23 @@
+// import React from 'react';
+import Card from './Card';
+
+const CardList = ({robots}) =>{
+	// if(true){
+	// 	throw new Error('Noooooo!');
+	// }
+	return (
+		<div>
+			{robots.map((user,i) =>{
+				return <Card 
+				key={robots[i].id} 
+				id={robots[i].id} 
+				name={robots[i].name} 
+				email={robots[i].email}
+				/>
+			})
+			}
+		</div>
+	);
+}
+
+export default CardList;
